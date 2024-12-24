1ecde890373ed346a82886cda084eb47139c66f3
Josiah Winslow <winslowjosiah@gmail.com>
2024-12-24 13:54:02 -0600

    Implement an actual clique algorithm for 2024 Day 23

--- aoc/2024/day23/__init__.py
+++ aoc/2024/day23/__init__.py
@@ -1,5 +1,6 @@
-from collections.abc import Iterable
+from collections.abc import Iterable, Iterator, Mapping
 import itertools as it
+from typing import TypeVar
 
 
 def get_computers(lines: Iterable[str]) -> dict[str, set[str]]:
@@ -31,40 +32,59 @@ def aoc2024_day23_part1(lines: Iterable[str]) -> int:
     return len(groups)
 
 
+N = TypeVar("N")
+def find_maximal_cliques(
+        graph: Mapping[N, Iterable[N]],
+        clique: set[N] | None = None,
+        potential_nodes: set[N] | None = None,
+        excluded_nodes: set[N] | None = None,
+) -> Iterator[set[N]]:
+    """
+    Find all maximal cliques (cliques that cannot be extended by adding
+    another node) in a graph.
+
+    This function uses the Bron-Kerbosch algorithm with pivoting.
+    """
+    if clique is None:
+        clique = set()
+    if potential_nodes is None:
+        potential_nodes = set(graph.keys())
+    if excluded_nodes is None:
+        excluded_nodes = set()
+
+    # Yield this clique if it's maximal (no nodes could be added to it)
+    if not potential_nodes and not excluded_nodes:
+        yield clique
+        return
+
+    # NOTE We can choose a "pivot" node, and only explore its
+    # non-neighbors as potential nodes to add to the clique. We will
+    # still find all maximal cliques this way; otherwise, if only
+    # neighbors of the pivot were in the maximal clique, we could add
+    # the pivot to it, which contradicts its maximality.
+    pivot = (potential_nodes | excluded_nodes).pop()
+
+    # For each node that could be added to the clique
+    for node in potential_nodes - set(graph[pivot]):
+        neighbors = set(graph[node])
+        # Find all cliques that also include this node
+        yield from find_maximal_cliques(
+            graph,
+            clique=clique | {node},
+            potential_nodes=potential_nodes & neighbors,
+            excluded_nodes=excluded_nodes & neighbors,
+        )
+        # Don't add this node to future cliques
+        potential_nodes.remove(node)
+        excluded_nodes.add(node)
+
+
 def aoc2024_day23_part2(lines: Iterable[str]) -> str:
     computers = get_computers(lines)
-    groups: set[frozenset[str]] = set()
-    seen: set[frozenset[str]] = set()
-    for name_a, connections_a in computers.items():
-        for name_b in connections_a:
-            # Start with two computers that are connected
-            group = {name_a, name_b}
-            # Skip if we've already made a group starting with these two
-            # computers
-            if frozenset(group) in seen:
-                continue
-            seen.add(frozenset(group))
-
-            # The other computers in the group must be connected with
-            # both of the first computers
-            group_connections = connections_a & computers[name_b]
-            # While there are computers left to process
-            while group_connections:
-                conn = group_connections.pop()
-                if conn in group:
-                    continue
-                # If this computer is connected to every other computer
-                # in the group
-                if all(conn in computers[cpu] for cpu in group):
-                    # It belongs in the group
-                    group.add(conn)
-                    # The computers it's connected to might belong in
-                    # the group
-                    group_connections.update(computers[conn])
-            groups.add(frozenset(group))
-
-    # Get the largest group by length, sorted alphabetically
-    return ",".join(sorted(max(groups, key=len)))
+    cliques = find_maximal_cliques(computers)
+    # Get the maximum clique, sorted alphabetically
+    # NOTE A "maximum clique" is a maximal clique with the largest size.
+    return ",".join(sorted(max(cliques, key=len)))
 
 
 parts = (aoc2024_day23_part1, aoc2024_day23_part2)
